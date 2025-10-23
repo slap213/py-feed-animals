9b5e457190fa45dc800f7cd208d923158a2917f7
Bartosz Chudziński <bartoszchudzinski95@gmail.com>
2025-10-23 18:01:18 +0200

    Refactor feed method and feed_animals function based on review

--- app/main.py
+++ app/main.py
@@ -13,8 +13,7 @@ class Animal:
             print(f"Eating {self.appetite} food points...")
             self.is_hungry = False
             return self.appetite
-        else:
-            return 0
+        return 0
 
 
 class Cat(Animal):
@@ -34,7 +33,4 @@ class Dog(Animal):
 
 
 def feed_animals(animals: list) -> int:
-    total_food = 0
-    for animal in animals:
-        total_food += animal.feed()
-    return total_food
+    return sum(animal.feed() for animal in animals)
